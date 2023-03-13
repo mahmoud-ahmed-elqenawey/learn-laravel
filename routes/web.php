f4506cbbce6921458119c69cb67a6b99e715b64a
mahmoudahmed2 <mahmoud.ahmed@ix-sol.com>
2023-03-13 16:37:52 +0200

    route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,34 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// index page
 Route::get('/', function () {
     return "<h1>Hi</h1>";
+});
+
+// route to page
+Route::get('about', function () {
+    return "<h1>About</h1>";
+});
+
+// route redirect by form to page use methoud post
+Route::get('contact', function () {
+    return '<form action="data" method="post" >
+        ' . csrf_field() . '
+        <input type="submit" value="go" />
+    </form>';
+});
+
+
+// rout with methoud post
+Route::post("data", function () {
+    return "data";
+});
+
+Route::prefix('product')->group(function () {
+    Route::get('/', fn () => "hi products");
+
+    Route::get('edit/{id}', fn ($id) =>  ('hi id:' . $id));
+    
+    Route::get('add', fn () => "<h1>add</h1>");
 });
